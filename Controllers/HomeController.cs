d18a37af712b2ea364a7ba9a2792445d511fe8db
daniel <daniel_chavez_10@hotmail.com>
2016-08-30 14:20:54 -0500

    inicial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace mvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName();
+			var isMono = Type.GetType("Mono.Runtime") != null;
+
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View(new Persona());
+		}
+
+		public ActionResult Process(Persona persona)
+		{
+			persona.Nombre += "X";
+			return View("Process", persona);
+		}
+	}
+}
+
---
+++ Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace mvc
+{
+	public class Persona
+	{
+		public string Nombre { get; set; }
+		public string Edad { get; set; }
+
+		public Persona()
+		{
+			Nombre = "Daniel";
+			Edad = "36";
+		}
+	}
+}
+
